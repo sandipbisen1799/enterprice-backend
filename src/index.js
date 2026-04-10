45324d078b363d73bfab5572cef290b66741063d
sandipbisen1799 <sandipbisen1799@gmail.com>
2026-04-10 15:53:19 +0530

    feat: initialize express application with middleware, CORS configuration, and core API routes

--- src/index.js
+++ src/index.js
@@ -16,7 +16,7 @@ const app = express();
 
 app.use(
   cors({
-       origin: ["http://localhost:5173", "http://localhost:5174"],
+       origin: ["http://localhost:5173", "http://localhost:5174", "https://enterprice-frontend.vercel.app"],
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
     allowedHeaders: ["Content-Type", "Authorization"],
